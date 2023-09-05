efac522b8d27ee4628f9cb674d8f8ab4b67e1326
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2023-09-05 19:18:34 +0300

    final

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,12 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
 import { LoginService } from '../services/login/login.service';
-
+import { environment } from 'src/environments/environment';
 
 export const isLoggedGuard : CanActivateFn = ()=>{
 
   const router = inject(Router);
   const loginService =  inject(LoginService);
+  const api = environment.apiUrl
   let userToken: string ;
 
   loginService.$token.subscribe(token=>{
@@ -15,7 +16,7 @@ export const isLoggedGuard : CanActivateFn = ()=>{
   })
 
   if(!userToken){
-router.navigate(['/login'])
+router.navigate([`${api}users/login`])
 return false
   }
 
